5598a7e7f30c692911719708c69916d15fab8036
Harry Sharma <harrysharma1066@gmail.com>
2023-06-08 20:01:07 +0100

    Add ffmpeg conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import {useState, useEffect} from 'react'
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
+import Skeleton from '@mui/material/Skeleton';
+
 function App() {
   const ffmpeg=createFFmpeg({log:true})
   const [ready, setReady]=useState(false)
@@ -12,7 +14,7 @@ function App() {
 
   useEffect(()=>{
     load();
-  },[])
+  },[video])
 
   const convertToGif=async()=>{
     ffmpeg.FS('writeFile', 'test.mp4',await fetchFile(video))
@@ -25,20 +27,35 @@ function App() {
     setGif(url)
   }
 
-  return ready?(<>
-  <div>
-    <input type='file' accept='video/*' onChange={(e)=>setVideo(e.target.files?.item(0))}/>
-    {video &&
-    <div>
-        <video controls width="250" src={URL.createObjectURL(video)}/>
-        <button className="bg-slate-400 mt-10 outline-1 hover:bg-slate-600" onClick={convertToGif}>Convert</button>
+  return (
+    <div className="bg-slate-500  w-full h-full absolute items-center justify-center">
+      <div className="flex flex-col">
+         {
+        ready ? (<>
+        <div className="flex flex-row space-x-5">
+             <div className="flex flex-col">
+                <h1 className="text-white">Video:</h1>
+               {video && <video controls width="250" src={URL.createObjectURL(video)}></video>}
+              </div> 
+              <div className="flex flex-col">
+                <h1 className="text-white">GIF:</h1>
+               {gif && <img src={gif} width="250" />}
+              </div> 
+             
+        </div>
+     
+        <input type="file" className="mt-5" onChange={(e)=>setVideo(e.target.files?.item(0))}/>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 w-32 h-12" onClick={convertToGif}>Convert</button>
+      
+        
+        </>):(<>Loading...</>)
+
+      }
+      </div>
+     
+
     </div>
-   
-    }
-    {gif && <img src={gif} width="250" />}
-  </div>
-  
-  </>):(<>Loading...</>)
+  )
 }
 
 export default App
